0df6c6fe4b525beb6089a2fc943309fb3862d2f2
Jeremy Freedman <jmf365@cornell.edu>
2018-11-15 16:32:59 -0500

    Moved db.py to src folder

--- src/db.py
+++ src/db.py
@@ -5,4 +5,4 @@ db = SQLAlchemy()
 class Team(db.Model):
     __tablename__ = "team"
     name = db.Column(db.Text, primary_key=True)
-    
+    img_url = db.Columb(db.Text, nullable=True) # can we host images on Google Cloud? 
