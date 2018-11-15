32e69604ff7ef8d4ac65602fbc224fbf7bb3058d
Jeremy Freedman <jmf365@cornell.edu>
2018-11-15 16:33:59 -0500

    testing vscode git

--- src/db.py
+++ src/db.py
@@ -6,3 +6,4 @@ class Team(db.Model):
     __tablename__ = "team"
     name = db.Column(db.Text, primary_key=True)
     img_url = db.Columb(db.Text, nullable=True) # can we host images on Google Cloud? 
+    # testing commit from vscode
