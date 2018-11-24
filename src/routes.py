0cd0fafda06c70ac93a2f9c88c421a7b7063e4f6
Jeremy Freedman <jmf365@cornell.edu>
2018-11-24 16:42:27 -0500

    added POST route for basic data insertion

--- src/routes.py
+++ src/routes.py
@@ -20,6 +20,19 @@ def get_teams():
     res = {'success': True, 'data': [team.serialize() for team in teams]} 
     return json.dumps(res), 200
 
+@app.route('/api/team/', methods=['POST'])
+def add_team():
+    response = json.loads(request.data)
+    team = Team(
+        name = response.get('name'),
+        img_url = response.get('img_url'),
+        description = response.get('description')
+    )
+    print(team.serialize())
+    db.session.add(team)
+    db.session.commit()
+    return json.dumps({'success': True, 'data': team.serialize()}), 201
+
 @app.route('/api/team/<string:teamname>/summary/')
 def get_summary(teamname):
     team = Team.query.filter_by(name=teamname).first()
@@ -50,6 +63,6 @@ def get_socials(teamname):
         socials = [social.serialize() for social in socials]
         return json.dumps({'success': True, 'data': socials}), 200
     return json.dumps({'success': False, 'error': 'Team not found!'}), 404
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
-#todo: members, socials
