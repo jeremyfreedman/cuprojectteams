2e1fdfea86020899c502d67b1c5859b7635eaa0c
jbhbrown <jdb393@cornnell.edu>
2018-12-01 16:20:54 -0500

    design/color, NetworkManager

--- src/ProjectTeams/ProjectTeams/NetworkManager.swift
+++ src/ProjectTeams/ProjectTeams/NetworkManager.swift
@@ -4,6 +4,7 @@
 //
 //  Copyright © 2018 CS 1998. All rights reserved.
 //
+// google cloud ip: 35.185.52.186
 
 import Foundation
 import Alamofire
@@ -20,14 +21,37 @@ enum SearchType {
 
 class NetworkManager {
     
-    private static let URL = ""
+    private static let URL = "35.185.52.186/api/"
     var teamname = ""
     
-    static func getSummary(fromProjectTeams teamname: [String], _ didGetProjectTeams: @escaping ([ProjectTeam]) -> Void) {
+    static func getSummary(fromProjectTeams summary: [String], _ didGetProjectTeams: @escaping ([ProjectTeam]) -> Void) {
         let parameters: [String:Any] = [
-            "teamname": teamname
+            "description": summary
         ]
-        Alamofire.request(URL, method: .post, /*parameters: Parameters,*/ encoding: URLEncoding.default, headers: [:]).validate().responseData { (response) in
+        Alamofire.request(URL, method: .get).validate().responseData { (response) in
+            switch response.result {
+            case .success(let data):
+                if let json = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) {
+                    print(json)
+                }
+                let decoder = JSONDecoder()
+                if let projectTeamsSearchResponse = try? decoder.decode(ProjectTeamSearchResponse.self, from: data) {
+                    print(projectTeamsSearchResponse.results)
+                    didGetProjectTeams(projectTeamsSearchResponse.results)
+                } else {
+                    print("Invalid Response Data")
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    static func getImage(fromProjectTeams imageURL: [String], _ didGetProjectTeams: @escaping ([ProjectTeam]) -> Void) {
+        let parameters: [String:Any] = [
+            "img_url": imageURL
+        ]
+        Alamofire.request(URL, method: .get).validate().responseData { (response) in
             switch response.result {
             case .success(let data):
                 if let json = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) {
@@ -50,7 +74,7 @@ class NetworkManager {
         let parameters: [String:Any] = [
             "teamname": teamname
         ]
-        Alamofire.request(URL, method: .post, /*parameters: Parameters,*/ encoding: URLEncoding.default, headers: [:]).validate().responseData { (response) in
+        Alamofire.request(URL, method: .get).validate().responseData { (response) in
             switch response.result {
             case .success(let data):
                 if let json = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) {
@@ -73,7 +97,7 @@ class NetworkManager {
         let parameters: [String:Any] = [
             "teamname": teamname
         ]
-        Alamofire.request(URL, method: .post, /*parameters: Parameters,*/ encoding: URLEncoding.default, headers: [:]).validate().responseData { (response) in
+        Alamofire.request(URL, method: .get).validate().responseData { (response) in
             switch response.result {
             case .success(let data):
                 if let json = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) {
@@ -96,7 +120,7 @@ class NetworkManager {
         let parameters: [String:Any] = [
             "teamname": teamname
         ]
-        Alamofire.request(URL, method: .post, /*parameters: Parameters,*/ encoding: URLEncoding.default, headers: [:]).validate().responseData { (response) in
+       Alamofire.request(URL, method: .get).validate().responseData { (response) in
             switch response.result {
             case .success(let data):
                 if let json = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) {
@@ -119,7 +143,7 @@ class NetworkManager {
         let parameters: [String:Any] = [
             "teamname": teamname
         ]
-        Alamofire.request(URL, method: .post, /*parameters: Parameters,*/ encoding: URLEncoding.default, headers: [:]).validate().responseData { (response) in
+        Alamofire.request(URL, method: .get).validate().responseData { (response) in
             switch response.result {
             case .success(let data):
                 if let json = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) {
@@ -142,7 +166,7 @@ class NetworkManager {
         let parameters: [String:Any] = [
             "teamname": teamname
         ]
-        Alamofire.request(URL, method: .post, /*parameters: Parameters,*/ encoding: URLEncoding.default, headers: [:]).validate().responseData { (response) in
+        Alamofire.request(URL, method: .get).validate().responseData { (response) in
             switch response.result {
             case .success(let data):
                 if let json = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) {
